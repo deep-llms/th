189e10c19088671dc9b1fc3944068da6b362175f
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-26 23:59:45 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,3 +1,8 @@
-#2
-#check-data
--0
+#1
+#train-alpha-variants
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+sleep 3
+conda activate embeddings_hub
+sleep 3
+export WANDB_MODE=offline
+python run_smoke_tests.py --arms S3_a015 S3_a02 --save-token-ids --stop-at-step 6500
