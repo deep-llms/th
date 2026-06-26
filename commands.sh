ce967b108de93f77c5bbe6fd2a134d11976c6eec
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-26 06:02:28 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
-#2
-#download-and-sample
--0
+#1
+#verify-env
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+conda activate embeddings_hub
+python -c "import torch; print(torch.cuda.is_available(), torch.cuda.device_count(), torch.version.cuda)"
