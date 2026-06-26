51c6947ce18f997fefbfcfe282192ae0740d5b6b
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-26 23:50:43 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,3 +1,9 @@
-#2
-#train-alpha-variants
--0
+#1
+#check-data
+du -sh /opt/dlami/nvme/embhub_data/
+du -sh /opt/dlami/nvme/embhub_data/Qwen_Qwen3-0.6B/train/*
+du -sh /opt/dlami/nvme/embhub_data/Qwen_Qwen3-0.6B/eval/*
+echo "---"
+ls /opt/dlami/nvme/embhub_data/Qwen_Qwen3-0.6B/train/
+ls /opt/dlami/nvme/embhub_data/Qwen_Qwen3-0.6B/train/en/
+ls /opt/dlami/nvme/embhub_data/Qwen_Qwen3-0.6B/train/vi/
