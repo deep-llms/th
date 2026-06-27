b35dcf43d3b3c084d6555e6df0ea9c69a30b982b
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-27 19:40:36 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,2 @@
-#1
+#2 -a
 #train-baseline
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
-sleep 3
-conda activate embeddings_hub
-sleep 3
-export WANDB_MODE=offline
-export NCCL_NVLS_ENABLE=0
-bash scripts/train_qwen3_0.6b_baseline.sh
