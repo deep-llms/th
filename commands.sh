a2f5d76cf259aab146ec600a9cc074ef24020527
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-27 17:15:10 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,3 +1,6 @@
-#2
-#probe-alpha-variants-1
-  
+#1
+#probe-alpha-variants
+echo "-------"
+ls /opt/dlami/nvme/smoke_test_outputs/S3_a015/probe2_single_step*_RESULTS.md
+echo "-------"
+ls /opt/dlami/nvme/smoke_test_outputs/S3_a02/probe2_single_step*_RESULTS.md
