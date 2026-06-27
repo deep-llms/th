21333bee82689c4f984ce6a627a7b155e6a6b264
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-27 18:19:55 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,6 +1,2 @@
-#1
+#2
 #check-wandb-size
-du -sh /opt/dlami/nvme/smoke_test_outputs/S3_a015/wandb/
-du -sh /opt/dlami/nvme/smoke_test_outputs/S3_a02/wandb/
-ls /opt/dlami/nvme/smoke_test_outputs/S3_a015/wandb/
-ls /opt/dlami/nvme/smoke_test_outputs/S3_a02/wandb/
