191f3b6a737d21e70df2a928d8401d429a83432b
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-27 19:09:36 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
-#2 -f-/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-6500/trainer_state.json
+#2 -f-/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-5500/trainer_state.json -f-/opt/dlami/nvme/smoke_test_outputs/S3_a015/checkpoint-5000/trainer_state.json -f-/opt/dlami/nvme/smoke_test_outputs/S3_a015/checkpoint-2500/trainer_state.json
 #check-wandb-size
