7730e7671591bf6060a8af201640aaf97c8d3166
nguyenhuuthien <nguyenhuuthien88bk@gmail.com>
2026-06-29 11:59:41 +0000

    update

--- commands.sh
+++ commands.sh
@@ -1,2 +1,19 @@
-#2 +a -f-/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-1500/eval_ppl.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-3250/eval_ppl.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-5500/eval_ppl.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-6500/eval_ppl.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-1500/eval_benchmarks.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-3250/eval_benchmarks.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-5500/eval_benchmarks.json,/opt/dlami/nvme/smoke_test_outputs/S3_a02/checkpoint-6500/eval_benchmarks.json
-#eval-results-s3a02
+#1
+#train-s3a02-long
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+sleep 3
+conda activate embeddings_hub
+sleep 3
+export WANDB_MODE=offline
+export NCCL_NVLS_ENABLE=0
+
+
+nvidia-smi
+sleep 3
+
+# Move old S3_a02 results to _old
+mv /opt/dlami/nvme/smoke_test_outputs/S3_a02 /opt/dlami/nvme/smoke_test_outputs/S3_a02_old
+mv /opt/dlami/nvme/smoke_test_outputs/S3_a02.log /opt/dlami/nvme/smoke_test_outputs/S3_a02_old.log
+sleep 3
+
+python run_smoke_tests.py --arms S3_a02 --save-token-ids --stop-at-step 30000
